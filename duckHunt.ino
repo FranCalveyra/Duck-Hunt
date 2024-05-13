c30a636fce6533119d9d31ec20e9dd95a7c7363b
FranC-prog <franciscocalveyra24@gmail.com>
2024-05-13 16:35:50 -0300

    Added comments

--- duckHunt.ino
+++ duckHunt.ino
@@ -1,39 +1,52 @@
+//Declaro las constantes, como los pines a usar y el tiempo total de juego
 const int finDeCarrera = 34;
 const int led = 26;
-bool patoFueGolpeado = false;
+const unsigned long tiempoTotalDeJuego = 30000; //En milisegundos
+//Declaro las variables
 unsigned long antesDePegar = 0;
 unsigned long tiempoAlPegar = 0;
 int puntaje;
+int dificultad;
+
 /*FALTA: 
 - Modularizar y escribir las cosas en funciones más chicas
 - Conectar con MQTT
 - Apagar el led si no le pegó en 5 segundos y restar 5 puntos
 */
 void setup() {
-  //Inicializo
+  //Inicializo las variables y los dispositivos
   Serial.begin(9600);
   pinMode(finDeCarrera, INPUT);
   pinMode(led, OUTPUT);
   antesDePegar = millis();
   puntaje = 0;
+  //Acá iría el código de MQTT relacionado con obtener la dificultad del próximo juego, validando al jugador, etc.
+  //Por ahora, la dificultad estará hardcodeada en 0
+  dificultad = 0;
 }
 
 void loop() {  
-  // Lectura del botón
+  ejecutarJuego(dificultad);
+
+}
+
+void ejecutarJuego(int nivelDeDificultad){
   int FDC = digitalRead(finDeCarrera);
   bool patoGolpeado = FDC == LOW;
   bool activoParaPegar = true;
+  int intervaloMaximo = obtenerIntervalo(nivelDeDificultad);
+
   if(activoParaPegar){
     digitalWrite(led, HIGH);
     if(patoGolpeado){
       tiempoAlPegar = millis();
-      if(tiempoAlPegar - antesDePegar> 5000 ){ //Delay parametrizado con la dificultad
+      if(tiempoAlPegar - antesDePegar> intervaloMaximo ){ //Delay parametrizado con la dificultad
         Serial.println("No le pegaste a tiempo, -5 puntos"); //Alternar puntaje 
-        puntaje-=5;
+        cambiarPuntaje(puntaje,-5);
       }
       else{
         Serial.println("Golpeaste al pato! +5 puntos");
-        puntaje+=5;
+        cambiarPuntaje(puntaje,5);
       }
       activoParaPegar = false;
       digitalWrite(led, LOW);
@@ -42,23 +55,21 @@ void loop() {
     }
   }
   unsigned long tiempoDeJuego = millis();
-  if(tiempoDeJuego - antesDePegar >= 20000){ //Después de 10 segundos, terminaría
+  if(tiempoDeJuego - antesDePegar >= tiempoTotalDeJuego){ //Después de 30 segundos, terminaría
     Serial.println("Tu puntaje fue: ");
     Serial.println(puntaje);
     antesDePegar = millis(); //Reinicio el tiempo de juego
     puntaje = 0;
     //Enviar el puntaje por MQTT al server
   }
+}
 
-  
-  
-  /*
-  - el led está prendido
-  - le pego al pato
-  - si le pego antes de los 5 segundos, sumo 5 puntos. Sino, resto 5 puntos
-  */
+int obtenerIntervalo(int nivelDeDificultad){
+  return 5000 - 1000*nivelDeDificultad; //Máximo nivel: 3
+}
 
-
+void cambiarPuntaje(int puntos, int delta){
+  puntos +=delta;
 }
 
 
